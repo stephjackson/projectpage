5f2adea99066a1e97430275e60618ca17727e047
stephjackson <steph.garrett.jackson@gmail.com>
2018-01-12 00:21:09 -0500

    Made it pretty

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require("express");
 var app = express();
+var bodyParser    = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res){
-    res.send('This is a test!');
+    res.render("landing");
 });
 
 app.listen(3000, function(){
