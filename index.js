b42ada1b329cf5237d357b89d452b42f674ce23a
stephjackson <steph.garrett.jackson@gmail.com>
2018-01-16 15:35:17 -0500

    Adds a simple jQuery Game Boy sprite editor, fixes landing page text width, makes pass at fixing mobile text size.

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
+app.get("/gbsketch", function(req, res){
+    res.render("sketch");
+});
+
 app.listen(3000, function(){
     console.log('Server is up on 3000');
 });
---
+++ public/src/sketch.js
@@ -0,0 +1,22 @@
+$(".container").on("click", ".box", function(){
+  if($(this).css("background-color") === "rgb(155, 188, 15)")
+  {
+    $(this).css("background-color", "rgb(139, 172, 15)");
+  }
+  else if($(this).css("background-color") === "rgb(139, 172, 15)")
+  {
+    $(this).css("background-color", "rgb(48, 98, 48)");
+  }
+  else if($(this).css("background-color") === "rgb(48, 98, 48)")
+  {
+    $(this).css("background-color", "rgb(15, 56, 15)");
+  }
+  else if($(this).css("background-color") === "rgb(15, 56, 15)")
+  {
+    $(this).css("background-color", "rgb(155, 188, 15)");
+  }
+  else
+  {
+    console.log("Something is wrong with the color picking function!");
+  }
+});
