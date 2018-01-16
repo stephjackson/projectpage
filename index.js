cbddeaff3d3c533bfa76c30bd34235674244ecc0
stephjackson <steph.garrett.jackson@gmail.com>
2018-01-16 16:12:56 -0500

    loads jquery app if i make it landing page, going to try power cycling

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res){
-    res.render("sketch");
+    res.render("landing");
 });
 
 app.get("/gbsketch", function(req, res){
