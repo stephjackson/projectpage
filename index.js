a8e2eab5d382fa2b9224d340af3c36054a51c7f8
stephjackson <steph.garrett.jackson@gmail.com>
2018-03-28 14:23:22 -0400

    Adding project links to profile page, will be revamping this significantly soon.

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ var port = process.env.PORT || 8080;
 var bodyParser = require("body-parser");
 var howler = require("howler");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function(req, res){
+app.get("/", (req, res) => {
     res.render("landing");
 });
 
-app.get("/gbsketch", function(req, res){
+app.get("/gbsketch", (req, res) => {
     res.render("sketch");
 });
 
-app.listen(port, function(){
+app.listen(port, () => {
     console.log('Server is up on ' + port);
 });
